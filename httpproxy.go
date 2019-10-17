11ebba9a6272c493764af3c3ea899139c4117217
edwardowens <edward.owens@gotinder.com>
2019-10-17 11:42:27 -0700

    feat(client): Adding more logging

--- httpproxy.go
+++ httpproxy.go
@@ -18,16 +18,6 @@ import (
 	"github.com/tinder-edwardowens/go-http-tunnel/proto"
 )
 
-type logResponseWriter struct {
-	http.ResponseWriter
-	logger log.Logger
-}
-
-func (l *logResponseWriter) WriteHeader(code int) {
-	l.logger.Log("level", 3, "msg", "response received", "status", code)
-	l.ResponseWriter.WriteHeader(code)
-}
-
 // HTTPProxy forwards HTTP traffic.
 type HTTPProxy struct {
 	httputil.ReverseProxy
@@ -128,10 +118,7 @@ func (p *HTTPProxy) Proxy(w io.Writer, r io.ReadCloser, msg *proto.ControlMessag
 	setXForwardedFor(req.Header, msg.RemoteAddr)
 	req.URL.Host = msg.ForwardedHost
 
-	p.ServeHTTP(&logResponseWriter{
-		ResponseWriter: rw,
-		logger:         p.logger,
-	}, req)
+	p.ServeHTTP(rw, req)
 	p.logger.Log(
 		"level", 3,
 		"msg", "response received",
